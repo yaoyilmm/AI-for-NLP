03ad938d2faf58e5cb72e40ff3dba1d9c5af27f2
yaoyilmm <416105839@qq.com>
2019-07-06 18:55:36 +0800

    选做题

--- lesson1/OptionalPatternMatch.py
+++ lesson1/OptionalPatternMatch.py
@@ -95,10 +95,12 @@ def is_match(rest, saying):
 #print(pat_match_with_seg('?*P is very good and ?*X'.split(), "My dog is very good and my cat is very cute".split()))
 
 response_pair = {
+    "?*X want ?*Y agin" : ["why do you want ?Y"],
     "I was ?*X": ["Were you really ?X ?", "I already knew you were ?X ."],
     'I need ?X': ["Why do you neeed ?X"],
     "I dont like my ?X": ["What bad things did ?X do for you?"],
     "?*X hello ?*Y": ["Hi, how do you do?"]
+    
     
 }
 
@@ -113,11 +115,7 @@ s =  pat_to_dict(pat_match_with_seg('?*X hello ?*Y'.split(),"I am mike hello lil
 match =" ".join(subsitite("Hi, how do you do? ?*X hello ?*Y".split(), pat_to_dict(pat_match_with_seg('?*X hello ?*Y'.split(), 
                   "I am mike hello lilei ".split()))))
 #print(match)
-rules = {
-    '?*x你好?*y': ['你好呀', '请告诉我你的问题'],
-    '?*x我想?*y': ['你觉得?y有什么意义呢？', '为什么你想?y', '你可以想想你很快就可以?y了']
 
-}
 def get_response(saying ,pattern):
     print("A:" + saying)
     for key in pattern.keys():
@@ -125,11 +123,17 @@ def get_response(saying ,pattern):
         #print(match_res)
         if match_res == fail:
             continue
+        
         match_dic =  pat_to_dict(match_res)
+       # print(match_dic)
+        keyReplace = ''.join(subsitite(key.split(),match_dic))
+        if keyReplace.replace(' ','') != saying.replace(' ',''):
+            continue
         temDic = {}
         for k,v in match_dic.items():
             if is_pattern_segment(k) :k = k.replace('?*','?')
             temDic[k] = v
+        
         result =  " ".join(subsitite(' '.join(pattern[key]).split(),temDic))
         print("B:"+ result + '\n')
         return result
@@ -138,4 +142,74 @@ get_response('I need money',response_pair)
 get_response('I dont like my dog',response_pair)
 get_response('I was late',response_pair)
 get_response('I am hellen, hello ',response_pair)
+get_response('I want to go shopping agin',response_pair)
 
+#arr = cut('?*x我想?*y')
+#print(len(arr))
+
+def splitChinese(saying):
+    sayingList = cut(saying)
+    temArr = []
+    #print(sayingList)
+    i = 0
+    for v in sayingList:
+        if sayingList[i] == '?' and (i+ 2) < len(sayingList) and sayingList[i+ 1] == '*' and sayingList[i + 2].isalpha():
+            temArr.append('?*' + sayingList[i + 2])
+            i = i + 2
+        else: 
+             if sayingList[i] == '?' and (i+ 1) < len(sayingList) and sayingList[i + 1].isalpha():
+                temArr.append('?' + sayingList[i + 1])
+                i = i + 1
+             else:
+                temArr.append(sayingList[i])
+        i = i + 1
+        if i >= len(sayingList):
+            break
+    return temArr
+arrB = cut("刘敏我想赵晗");
+rules = {
+    '?*x你好?*y': ['你好呀', '请告诉我你的问题'],
+    '?*x我想?*y': ['你觉得?y有什么意义呢？'],
+    '?*x喜欢?*y': ['喜欢?y的哪里？', '?y有什么好的呢？', '你想要?y吗？'],
+    '?*x讨厌?*y': ['?y怎么会那么讨厌呢?', '讨厌?y的哪里？', '?y有什么不好呢？', '你不想要?y吗？'],
+    '?*xAI?*y': ['你为什么要提AI的事情？', '你为什么觉得AI要解决你的问题？'],
+    '?*x机器人?*y': ['你为什么要提机器人的事情？', '你为什么觉得机器人要解决你的问题？'],
+    '?*x对不起?*y': ['不用道歉', '你为什么觉得你需要道歉呢?']
+    
+
+}
+match = pat_match_with_seg(splitChinese('?*x我想?*y'), arrB)  
+#print(match)
+match ="".join(subsitite(splitChinese('?*x我想?*y'), pat_to_dict(match)))
+#print(match)
+
+def get_response_zh_cn(saying ,pattern):
+    print("A:" + saying)
+    for key in pattern.keys():
+        keyArr = splitChinese(key)
+        match_res = pat_match_with_seg(keyArr,cut(saying))
+        #print(match_res)
+        if match_res == fail:
+            continue
+        match_dic =  pat_to_dict(match_res)
+        #print(match_dic)
+        keyReplace = ''.join(subsitite(keyArr,match_dic))
+        if keyReplace.replace(' ','') != saying.replace(' ',''):
+            continue
+        temDic = {}
+        for k,v in match_dic.items():
+            if is_pattern_segment(k) :k = k.replace('?*','?')
+            temDic[k] = v
+        #print(temDic)
+        result =  "".join(subsitite(splitChinese(''.join(pattern[key])),temDic))
+        print("B:"+ result + '\n')
+        return result
+
+get_response_zh_cn("刘敏我想减肥",rules)
+
+get_response_zh_cn("亲爱的你好呀",rules)
+get_response_zh_cn("刘敏讨厌回家",rules)
+get_response_zh_cn("我对不起父母",rules)
+get_response_zh_cn("我喜欢他",rules)
+
+#print(splitChinese(''.join(['?y怎么会那么讨厌呢?', '讨厌?y的哪里？', '?y有什么不好呢？', '你不想要?y吗？'])))
